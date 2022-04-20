58b50d8db28481568d60f6fc2527e7b5d3d81dce
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2022-04-20 14:30:14 +0200

    [Feature] Add another example for estPair

--- td1.js
+++ td1.js
@@ -8,3 +8,14 @@ Matthisfunction estPair(a){
     }
     return resultat
 }
+
+function estPair (a){   //Création d'une fonction pour savoir si un nombre est pair
+    var resultat
+    if ( (a % 2) == 0 ){ //regarde si le reste de la division euclidienne est bien nulle et donc le nombre est pair
+        resultat = TRUE
+    }
+    else {
+        resultat = FALSE
+    }
+    return resultat
+}
